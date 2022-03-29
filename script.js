bda23e2586962a88190695dfa05f2f4d0dd3c52b
Jake Walsh <jkwalsh127@gmail.com>
2022-03-28 21:13:31 -0700

    change hero section image

--- script.js
+++ script.js
@@ -2,16 +2,17 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    document.getElementById("h1").style.fontSize = "200%";
-    document.getElementById("h1").style.height = "50px";
-    document.getElementById("h1").style.width = "200px";
+    document.getElementById("my-name").style.fontSize = "200%";
+    document.getElementById("my-name-label").style.height = "40px";
+    document.getElementById("my-name-label").style.width = "350px";
     document.getElementById("nav-ul").style.fontSize = "100%";
     document.getElementById("header").style.height = "50px";
   } else {
-    document.getElementById("h1").style.fontSize = "400%";
-    document.getElementById("h1").style.height = "80px";
-    document.getElementById("h1").style.width = "350px";
+    document.getElementById("my-name").style.fontSize = "400%";
+    document.getElementById("my-name-label").style.height = "70px";
+    document.getElementById("my-name-label").style.width = "350px"; 
+    document.getElementById("my-name").style.width = "350px";
     document.getElementById("nav-ul").style.fontSize = "150%";
-    document.getElementById("header").style.height = "150px";
+    document.getElementById("header").style.height = "100px";
   }
-}
+}       
